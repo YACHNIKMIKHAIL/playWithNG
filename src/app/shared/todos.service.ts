809df258c0096a5e8dde1edc6a5775850cec9c92
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-08-05 11:38:02 +0200

    fetch from svr is realised

--- src/app/shared/todos.service.ts
+++ src/app/shared/todos.service.ts
@@ -1,4 +1,6 @@
 import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable, tap} from "rxjs";
 
 export interface ITodo {
   id: number
@@ -10,12 +12,18 @@ export interface ITodo {
 @Injectable({providedIn: 'root'})
 export class TodosService {
 
-  public todos: ITodo[] = [
-    {id: 1, title: 'Some title 1', completed: false, date: new Date()},
-    {id: 2, title: 'Some title 2', completed: true, date: new Date()},
-    {id: 3, title: 'Some title 3', completed: false, date: new Date()},
-  ]
-  private newTitle: string = ''
+  constructor(private http: HttpClient) {
+  }
+
+  public todos: ITodo[] = []
+  public newTitle: string = ''
+
+  fetchTodos(): Observable<ITodo[]> {
+    return this.http.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos')
+      .pipe(
+        tap(todos => console.log(todos))
+      )
+  }
 
   onToggle(id: number) {
     const idx = this.todos.findIndex(t => t.id === id)
@@ -26,9 +34,8 @@ export class TodosService {
     this.todos = this.todos.filter(t => t.id !== id)
   }
 
-  newTodoTitle(v: any) {
-    console.log('newTodoTitle', v.target.value)
-    this.newTitle = v.target.value
+  newTodoTitle(v: Event) {
+    this.newTitle = (v.target as HTMLInputElement).value
   }
 
   addTodo() {
@@ -36,5 +43,6 @@ export class TodosService {
     this.todos.push({
       id: this.todos.length + 1, title: this.newTitle, completed: false, date: new Date()
     })
+    this.newTitle = ''
   }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -7,10 +7,14 @@ import {TodosService} from "../shared/todos.service";
   styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
+  public loading: boolean = true
+
   constructor(public todosService: TodosService) {
   }
 
   ngOnInit(): void {
+    this.todosService.fetchTodos()
+      .subscribe(() => this.loading = false)
   }
 
   onChange(id: number) {
